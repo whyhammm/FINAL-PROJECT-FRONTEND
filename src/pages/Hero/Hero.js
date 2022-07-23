7187d8e788b796a0e0e6cbf3f1acee8734b7829b
whyhammm <101931791+whyhammm@users.noreply.github.com>
2022-07-23 09:10:55 +0700

    Update Hero.js

--- src/pages/Hero/Hero.js
+++ src/pages/Hero/Hero.js
@@ -1,25 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import heroimg from "../../images/shallow-pan.svg";
+import Footer from "../../components/Footer/Footer";
 import "./Hero.css";
 const Hero = () => {
   return (
-    <div className="hero">
-      <img src={heroimg} alt="breakfast" />
-      <div className="content">
-        <h1>
-          Life is infinitely <span>Delicious</span>
-        </h1>
-        <p className="description">
-          Hunger is a good cook! Here are some classic recipes from all over
-          the world.
-        </p>
-        <Link to="/features">
-          <button className="btn-order">Browse Recipes</button>
-        </Link>
+    <>
+      <div className="hero">
+        <img src={heroimg} alt="breakfast" />
+        <div className="content">
+          <h1>
+            Life is infinitely <span>Delicious</span>
+          </h1>
+          <p className="description">Hunger is a good cook! Here are some classic recipes from all over the world.</p>
+          <Link to="/features">
+            <button className="btn-order">Browse Recipes</button>
+          </Link>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
+
 
 export default Hero;
